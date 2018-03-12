1db617184ea08ba1b11a459d80517884ac0ac63f
Layout example <x16137108@ncirl.ie>
2018-03-12 18:02:09 +0000

    setup parsed JSON array and output

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,9 +1,9 @@
 class Stock < ApplicationRecord
     
     def self.new_from_lookup(username)
-        lookedup_path = 'https://api.github.com/users/' + username
-        looked_up_stock = HTTParty.get(lookedup_path)
-        
+        lookedup_path = 'https://api.github.com/users/' + username + '/repos'
+        looked_up_stock1 = HTTParty.get(lookedup_path)
+        looked_up_stock = looked_up_stock1.parsed_response
     
        
         #looked_up_stock
