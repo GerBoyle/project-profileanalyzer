2a54101f403f3eee35ed419ca30f7e855813db34
Layout example <x16137108@ncirl.ie>
2018-03-13 11:48:54 +0000

    Added image to home page

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -2,8 +2,8 @@ class StocksController < ApplicationController
    
    def search
        @stock = Stock.new_from_lookup(params[:stock])
-       #render json: @profile
        render 'users/my_profile'
    end
     
+ 
 end
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,13 +1,10 @@
 class Stock < ApplicationRecord
     
     def self.new_from_lookup(username)
-        lookedup_path = 'https://api.github.com/users/' + username + '/repos'
-        looked_up_stock1 = HTTParty.get(lookedup_path)
-        looked_up_stock = looked_up_stock1.parsed_response
-    
-       
-        #looked_up_stock
+        lookedup_path = 'https://api.github.com/users/' + username
+        looked_up_stock = HTTParty.get(lookedup_path)
+        #looked_up_stock1 = looked_up_stock1.parsed_response
     end
-    
+
 
 end
