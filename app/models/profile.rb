3bc4794f467210aa94eba0e46f1793df868619dd
Layout example <x16137108@ncirl.ie>
2018-03-17 16:31:31 +0000

    added array loop for repo names

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,8 @@
 class Profile < ApplicationRecord
     
     def self.new_from_lookup(username)
-        lookedup_path = 'https://api.github.com/users/' + username
+        #lookedup_path = 'https://api.github.com/users/' + username
+        lookedup_path = 'https://api.github.com/users/' + username + '/repos'
         looked_up_profile = HTTParty.get(lookedup_path)
         #looked_up_profile1 = looked_up_profile.parsed_response
     end
