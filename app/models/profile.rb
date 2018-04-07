5ad4985b4b51ae32b9f2f0eb6d9d6a137e13eebd
Layout example <ger_boyle@yahoo.ie>
2018-04-07 02:23:24 +0000

    Heroku setup

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,7 +2,7 @@ class Profile < ApplicationRecord
    
     def self.new_from_lookupRepos(username)
         lookedup_path = 'https://api.github.com/users/' + username + '/repos'
-        auth = {:username => "GerBoyle", :password => "#"}
+        auth = {:username => "GerBoyle", :password => "*"}
         HTTParty.get(lookedup_path, :basic_auth => auth)
         
         
@@ -10,7 +10,7 @@ class Profile < ApplicationRecord
 
     def self.new_from_lookupUser(username)
         lookedup_path = 'https://api.github.com/users/' + username
-        auth = {:username => "GerBoyle", :password => "#"}
+        auth = {:username => "GerBoyle", :password => "*"}
         HTTParty.get(lookedup_path, :basic_auth => auth)
     end
 
