4aa2c0173514986ba4fa5ba6fe4a16cd00b19f2a
Layout example <ger_boyle@yahoo.ie>
2018-04-09 17:39:14 +0000

    updated

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,16 +2,16 @@ class Profile < ApplicationRecord
    
     def self.new_from_lookupRepos(username)
         lookedup_path = 'https://api.github.com/users/' + username + '/repos'
-
-        HTTParty.get(lookedup_path)
+        auth = {:username => "GerBoyle", :password => ENV['GITHUB_TOKEN']}
+        HTTParty.get(lookedup_path, :basic_auth => auth)
         
         
     end
 
     def self.new_from_lookupUser(username)
         lookedup_path = 'https://api.github.com/users/' + username
-        HTTParty.get(lookedup_path)
+        auth = {:username => "GerBoyle", :password => ENV['GITHUB_TOKEN']}
+        HTTParty.get(lookedup_path, :basic_auth => auth)
     end
-
 
 end
